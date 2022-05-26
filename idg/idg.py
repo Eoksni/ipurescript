70d9323fe70945caab06a7a5ae20cff470523fa9
Seçkin KÜKRER <seckin.kukrer@hotmail.com>
2022-05-27 01:50:47 +0300

    Finish(idg): Think simple, live long.
    This commit is a view of my changed perspective. As a compile-to-python
    language, Dg, has a special place.

--- idg/idg.py
+++ idg/idg.py
@@ -7,6 +7,7 @@ import os
 import signal
 import subprocess
 import sys
+import dg
 
 from ipykernel.kernelbase import Kernel
 from pexpect import popen_spawn
@@ -34,43 +35,13 @@ class IdgKernel(Kernel):
         # -this project will be shared among all kernels
         # +will do bro!
 
-        project_path = os.path.join(os.path.dirname(__file__), 'temp')
-        try:
-            os.makedirs(project_path)
-        except OSError as err:
-            if err.errno != errno.EEXIST:
-                raise
-
-        # I don't care about the Windows or any
-        # other Microsoft products at all! :))
-        exec_str = 'python -m dg'
-        subprocess.run([exec_str, 'init'], cwd=project_path)
-
-        self.child = popen_spawn.PopenSpawn(
-            exec_str, # yeap, no additional `-repl` str
-            cwd=project_path
-        )
-        self.child.expect('\n>>> ')
-
     def do_execute(self, code, silent, store_history=True, user_expressions=None,
                    allow_stdin=False):
         if not silent:
-            self.child.sendline(code)
-            # waiting for prompt to show up
-            self.child.expect('...')
-            response_text = bytes(self.child.before)
-            if response_text == b'' or response_text[-1] == b'\n'[-1]:
-                # means we got an actual prompt
-                pass
-            else:
-                # means we got just a '> ' characters in the middle of a line
-                # so we do another search until we find an actual prompt
-                response_text += self.child.after
-                self.child.expect('\n>>> ')
-                response_text += self.child.before
+            ret_ = eval(dg.compile(code))
 
             stream_content = {'name': 'stdout',
-                              'text': response_text.decode('utf-8')}
+                              'text': str(ret_)}
             self.send_response(self.iopub_socket, 'stream', stream_content)
 
         return {
@@ -79,6 +50,3 @@ class IdgKernel(Kernel):
             'payload': [],
             'user_expressions': {},
         }
-
-    def do_shutdown(self, restart):
-        self.child.kill(signal.CTRL_C_EVENT)
